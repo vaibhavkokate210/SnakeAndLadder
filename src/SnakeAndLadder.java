4f63bd967e9c589057580cae252f882395195611
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-10 10:26:18 +0530

    UC5 done

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -13,20 +13,29 @@ public class SnakeAndLadder {
 		option=(int) (Math.floor(Math.random()*10)%3);
 	    System.out.println(option);
 	    
-	    if(option==1)
-	      {
-	        	int max=position+number;
-		        if(max<=100)
-		        position=max;
-	      }
-	     else if(option==2)
-	     {
-		        int min=position-number;
-		        if(min>0)
-		          position=min;
-		        else
-			      position=0;
-	     }
+	    while(position!=100)
+		{
+		      number=(int) (Math.floor(Math.random()*10)%6);
+		      System.out.println(number);
+		
+	       	  option=(int) (Math.floor(Math.random()*10)%3);
+		      System.out.println(option);
+		
+		      if(option==1)
+		      {
+		        	int max=position+number;
+			        if(max<=100)
+			        position=max;
+		      }
+		     else if(option==2)
+		     {
+			        int min=position-number;
+			        if(min>0)
+			          position=min;
+			        else
+				      position=0;
+		     }
+		     System.out.println("Position = "+position);
+		  }
 	    }
-
 }
