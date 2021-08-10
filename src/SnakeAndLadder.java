3fd3bb09afce0e57c35b1dac9570605fee6c85b2
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-10 09:47:08 +0530

    option is added

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -5,8 +5,25 @@ public class SnakeAndLadder {
 	{
 		int position=0;
 		int number;
+		int option;
+
 		number=(int) (Math.floor(Math.random()*10)%6)+1;
 		System.out.println(number);
+		
+		option=(int) (Math.floor(Math.random()*10)%3);
+	    System.out.println(option);
+	    
+	    if(option==1)
+	      {
+	        	int max=position+number;
+		        position=max;
+	      }
+	     else if(option==2)
+	     {
+		        int min=position-number;      
+		          position=min;
+		 }
+	    System.out.println(position);
 	}
 
 }
