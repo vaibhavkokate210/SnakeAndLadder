3d4481e6c5465876cad5825e94d7ab9f8202c201
Vaibhav Kokate <vaibhavkokate210@gmail.com>
2021-08-10 09:21:00 +0530

    start position added

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,9 +1,9 @@
 
 public class SnakeAndLadder {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		int position=0;
 	}
 
 }
