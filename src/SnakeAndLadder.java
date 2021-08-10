281481e00e434fdc70faa0b4b4c27eee751c8cfb
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-10 09:35:19 +0530

    player rolls a die

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -4,6 +4,9 @@ public class SnakeAndLadder {
 	public static void main(String[] args) 
 	{
 		int position=0;
+		int number;
+		number=(int) (Math.floor(Math.random()*10)%6)+1;
+		System.out.println(number);
 	}
 
 }
